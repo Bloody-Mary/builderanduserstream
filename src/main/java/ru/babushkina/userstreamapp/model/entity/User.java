a9eaa8650432f70db7df40ebc17ac92fa7607bf1
Bloody-Mary <37978402+Bloody-Mary@users.noreply.github.com>
2023-08-03 12:19:45 +0300

    Created class User using pattern InnerBuilder

--- src/main/java/ru/babushkina/userstreamapp/model/entity/User.java
+++ src/main/java/ru/babushkina/userstreamapp/model/entity/User.java
@@ -1,4 +1,61 @@
 package ru.babushkina.userstreamapp.model.entity;
 
 public class User {
+    private Long id;
+    private String login;
+    private StringBuilder password;
+    private int age;
+
+    private User (Builder builder) {
+        this.id = builder.id;
+        this.login = builder.login;
+        this.password = builder.password;
+        this.age = builder.age;
+    }
+
+    public static class Builder {
+        private Long id;
+        private String login;
+        private StringBuilder password;
+        private int age;
+
+        public Builder() {
+            this.password = new StringBuilder();
+        }
+
+        public Builder setId (Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setLogin(String login) {
+            this.login = login;
+            return this;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public StringBuilder getPassword() {
+        return password;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
